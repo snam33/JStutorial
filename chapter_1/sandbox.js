517d3a87fab62aa81534ed3efc4405cfc675f491
Sol Nam <cbkpwr@gmail.com>
2024-09-05 09:30:40 -0500

    null and undefined

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,23 +1,3 @@
-let ninjas = ['shaun', 'ryu', 'chun-li'];
+let age = null;
 
-// ninjas[1] = 'ken'; // changed to ken
-// console.log(ninjas[1]);
-
-// let ages = [20,25,30,35];
-// console.log(ages[2]);
-
-// let random = ['shaun', 'crystal', 30, 20];
-// console.log(random);
-
-// console.log(ninjas.length);
-
-// array methods
-
-// let result = ninjas.join(',');
-// let result = ninjas.indexOf('chun-li');
-// let result = ninjas.concat(['ken', 'crystal']); //연결하다. 기존 어레이에 추가로 연결
-let result = ninjas.push('ken'); //return new array length
-result = ninjas.pop(); // array에서 push한거 뺴기
-
-console.log(ninjas);
-console.log(result);
+console.log(age, age + 3, `the age is ${age}`);
