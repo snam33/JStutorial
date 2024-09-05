14e0c4c2ac59d05e437af00713bf278cb7ad4d36
Sol Nam <cbkpwr@gmail.com>
2024-09-05 11:10:30 -0500

    boolean - comparisons

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,3 +1,25 @@
-let age = null;
+// booleans & comparisons
+console.log(true, false, "true", "false");
 
-console.log(age, age + 3, `the age is ${age}`);
+// methods can return booleans
+let email = 'luigi@thenetninja.co.uk';
+let names = ['mario', 'luigi', 'toad']
+
+// let result = email.includes('!');
+// let result = names.includes('luigi');
+
+// console.log(result);
+
+// comparison operators
+let age = 25;
+
+// loose comparison (different types can still be equal)
+
+// console.log(age == 25);
+// console.log(age == '25');
+
+// strict comparison (different types cannot be equal)
+
+console.log(age === 25);
+console.log(age === '25');
+
